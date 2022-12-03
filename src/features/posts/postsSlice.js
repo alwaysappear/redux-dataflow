a050867ae0760234ea312221347c8359edbfce04
Raheem Kawojue <kawojue08@gmail.com>
2022-12-04 00:41:51 +0100

    addpost reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,8 +9,12 @@ const postsSlice = createSlice({
     name: "posts",
     initialState,
     reducers: {
-
+        addPost: (state, action) => {
+            state.push(action.payload)
+        }
     }
 })
+
+export const selectAllPosts = state => state.posts
 
 export default postsSlice.reducer

85776f39b2dd48d4c13db1775cfbd8304384e915
Raheem Kawojue <kawojue08@gmail.com>
2022-12-04 19:43:58 +0100

    dispatched post

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,24 +1,38 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+import { addPost } from '../features/posts/postsSlice'
 
 const AddPost = () => {
+    const dispatch = useDispatch()
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
-    const onTitleChange = e => setTitle(e.target.value)
-    const onContentChange = e => setContent(e.target.value)
+    const onSave = e => {
+        e.preventDefault()
+        if (title && content) {
+            dispatch(addPost(title, content))
+        }
+        setTitle("")
+        setContent("")
+    }
 
     return (
         <section>
-            <h2>Add New Post</h2>
-            <form>
-                <label htmlFor="postTitle">Post Title: </label>
-                <input type="text" id="postTitle" value={title}
-                    onChange={(e) => onTitleChange(e)} />
-                <label htmlFor="postContent">Post Title: </label>
-                <textarea type="text" id="postContent" value={content}
-                    onChange={(e) => onContentChange(e)} />
+            <h2 className="header-2">add new post</h2>
+            <form onSubmit={e => onSave(e)} className="addpost-form">
+                <div>
+                    <label htmlFor="postTitle">post title: </label>
+                    <input type="text" id="postTitle" value={title}
+                        onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="postContent">post content: </label>
+                    <textarea type="text" id="postContent" value={content}
+                        onChange={(e) => setContent(e.target.value)} />
+                </div>
+                <button className="submit-btn">save post</button>
             </form>
-            <button className="" type="submit">save post</button>
         </section>
     )
 }

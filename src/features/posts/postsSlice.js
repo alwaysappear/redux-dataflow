4273e52402f64ecfbeed04360be62dc47cea9201
Raheem Kawojue <kawojue08@gmail.com>
2022-12-04 21:58:18 +0100

    Update posts slice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,20 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
-    { id: 1, title: "Learning Redux Toolkit", content: "I have been enjoying it." },
-    { id: 2, title: "Learning Frontend", content: "I have not encountered literally anything hard since I have been learning." }
+    { id: 1, title: "Learning Redux Toolkit",
+    content: "I have been enjoying it." },
+    { id: 2, title: "Learning Frontend",
+    content: "I have not encountered literally anything hard since I have been learning." }
 ]
 
 const postsSlice = createSlice({
     name: "posts",
     initialState,
     reducers: {
-        addPost: (state, action) => {
-            state.push(action.payload)
+        addPost: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content
+                    }
+                }
+            }
         }
     }
 })
 
-export const selectAllPosts = state => state.posts
-
 export default postsSlice.reducer
+export const { addPost } = postsSlice.actions
+export const selectAllPosts = state => state.posts

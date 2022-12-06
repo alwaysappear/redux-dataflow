3aadb139f2321d63bb9def268e48a2faaed76376
Raheem Kawojue <kawojue08@gmail.com>
2022-12-06 13:20:57 +0100

    more func

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,25 +1,38 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addPost } from '../features/posts/postsSlice'
+import { selectAllUsers } from '../features/users/usersSlice'
 
 const AddPost = () => {
     const dispatch = useDispatch()
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
+    const users = useSelector(selectAllUsers)
 
-    const onSave = e => {
-        e.preventDefault()
-        if (title && content) {
-            dispatch(addPost(title, content))
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [userId, setUserId] = useState("")
+
+    const onSave = () => {
+        if (valid) {
+            dispatch(addPost(title, content, userId))
         }
+
         setTitle("")
         setContent("")
+        setUserId("")
     }
+
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
+
+    const valid = Boolean(title) && Boolean(content) && Boolean(userId)
 
     return (
         <section>
             <h2 className="header-2">add new post</h2>
-            <form onSubmit={e => onSave(e)} className="addpost-form">
+            <form className="addpost-form">
                 <div>
                     <label htmlFor="postTitle">post title: </label>
                     <input type="text" id="postTitle" value={title}
@@ -30,7 +43,18 @@ const AddPost = () => {
                     <textarea type="text" id="postContent" value={content}
                         onChange={(e) => setContent(e.target.value)} />
                 </div>
-                <button className="submit-btn">save post</button>
+                <div>
+                    <label htmlFor="authtor">author: </label>
+                    <select value={userId} id="authtor"
+                        onChange={(e) => setUserId(e.target.value)}>
+                        <option>Select author</option>
+                        {usersOptions}
+                    </select>
+                </div>
+                <button className="submit-btn disabled:opacity-[50%]"
+                    disabled={!valid} onClick={() => onSave()}>
+                    save post
+                </button>
             </form>
         </section>
     )
